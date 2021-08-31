2be6dc6442d94fa1fda9161c6b665a164b1dc77f
Oryanab <86202122+Oryanab@users.noreply.github.com>
2021-08-31 23:19:30 +0300

    exercise6 + complete

---
+++ exersice6.js
@@ -0,0 +1,48 @@
+const formula1Champions = [
+  "Schumacher",
+  "Schumacher",
+  "Schumacher",
+  "Schumacher",
+  "Schumacher",
+  "Alonso",
+  "Alonso",
+  "Räikkönen",
+  "Hamilton",
+  "Button",
+  "Vettel",
+  "Vettel",
+  "Vettel",
+  "Vettel",
+  "Hamilton",
+  "Hamilton",
+  "Rosberg",
+  "Hamilton",
+  "Hamilton",
+  "Hamilton",
+  "Hamilton",
+];
+
+let orderedWinners = [];
+formula1Champions.sort();
+for (let n = 0; n < formula1Champions.length - 1; n++) {
+  if (formula1Champions[n] !== formula1Champions[n - 1]) {
+    orderedWinners.push(formula1Champions[n]);
+  }
+}
+
+let winners = {};
+function countWin(driver) {
+  let timeswon = [];
+  for (let c = 0; c < formula1Champions.sort().length - 1; c++) {
+    if (formula1Champions[c] === driver) {
+      timeswon.push(formula1Champions[c]);
+    }
+  }
+
+  return timeswon.length;
+}
+
+for (let i = 0; i < orderedWinners.length; i++) {
+  winners[orderedWinners[i]] = countWin(orderedWinners[i]);
+}
+console.log(winners);
